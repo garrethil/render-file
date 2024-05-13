b67284196d5914f0ed5f750a45b5120cb0b99af2
garrethil <garrethildebrandt@gmail.com>
2024-05-13 12:50:36 -0400

    add resolvers for queries and mutations on server side

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const User = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (_, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+
+      throw AuthenticationError;
+    },
+    user: async (_, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+  Mutation: {
+    addUser: async (_, { username, email, password, onMailList }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
