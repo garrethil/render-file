f6b93ad15cf8dd7839aa68c05af921404bb3d231
garrethil <garrethildebrandt@gmail.com>
2024-06-14 16:06:38 -0400

    remove image from home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col items-center p-4 md:p-6 lg:p-8">
       <Schedule />
-      <h3 className="my-5 p-3 text-lg sm:text-xl lg:text-2xl text-center">
-        It will look like this
-      </h3>
-      <img
-        src="/renderFile3.png"
-        alt="Render File Event"
-        className="mx-5 mb-4 border-4 border-gray-300 rounded w-full md:w-2/3 lg:w-1/2"
-      />
     </div>
   );
 }
