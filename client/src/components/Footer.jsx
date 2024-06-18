c681c4a0789d223a830545f7f68fb8a9d0d750b9
garrethil <garrethildebrandt@gmail.com>
2024-06-17 20:12:50 -0400

    style footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-headerBG text-center p-4 mt-auto">
       <div className="container mx-auto">
         <p className="text-sm">© {new Date().getFullYear()} Render File.</p>
-        <div className="flex justify-center space-x-4 mt-2">
+        <div className="flex justify-center space-x-4 my-4">
           <a href="/about" className="hover:underline">
             About
           </a>
