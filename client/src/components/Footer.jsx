e1b4e569eb3961a2366c10fb29b7d5f1486509aa
garrethil <garrethildebrandt@gmail.com>
2024-06-17 20:09:23 -0400

    add to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => {
             Youtube
           </a>
         </div>
-        <div className="absolute bottom-0 w-full text-center">
-          <p className="text-xs text-gray-400">website by garret hildebrandt</p>
-        </div>
+      </div>
+      <div className="flex justify-center p-3 my-3">
+        <p className="text-xs text-gray-400">website by garret hildebrandt</p>
       </div>
     </footer>
   );
