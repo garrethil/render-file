90d4b9c1f7612deff7def70561adcdf8c8df0344
garrethil <garrethildebrandt@gmail.com>
2024-07-09 11:25:43 -0400

    update data and footer

--- client/src/assets/Data.js
+++ client/src/assets/Data.js
@@ -1,11 +1,5 @@
 const events = [
   {
-    id: 3,
-    date: "July 3rd @8pm",
-    location: "Wenona Beerlodge",
-    game: "Birthday Bash: Mario Kart Madness",
-  },
-  {
     id: 4,
     date: "July 10th @8pm",
     location: "Wenona Beerlodge",
@@ -15,13 +9,19 @@ const events = [
     id: 5,
     date: "July 17th @8pm",
     location: "Wenona Beerlodge",
-    game: "TBD",
+    game: "Indie Dev Games",
   },
   {
     id: 2,
     date: "July 24th @8pm",
     location: "Wenona Beerlodge",
     game: "TBD",
+  },
+  {
+    id: 3,
+    date: "July 31st @8pm",
+    location: "Wenona Beerlodge",
+    game: "Birthday Bash: Mario Kart Madness",
   },
 ];
 
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,23 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
   return (
     <footer className="bg-headerBG text-center p-4 mt-auto">
       <div className="container mx-auto">
         <p className="text-sm">© {new Date().getFullYear()} Render File.</p>
         <div className="flex justify-center space-x-4 my-4">
-          <a href="/about" className="hover:underline">
+          <a href="/about" className="hover:underline text-lg">
             About
           </a>
           <a
             href="https://www.youtube.com/@render_file"
             target="_blank"
-            className="hover:underline"
+            rel="noopener noreferrer"
+            className="hover:underline text-lg"
           >
-            Youtube
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+          <a
+            href="https://www.instagram.com/render.file"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline text-lg"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
         </div>
       </div>
       <div className="flex justify-center pt-3 mt-3">
-        <p className="credits text-gray-400">by garret hildebrandt</p>
+        <address className="credits text-gray-400">
+          by Garret Hildebrandt
+        </address>
       </div>
     </footer>
   );
