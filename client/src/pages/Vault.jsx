6c4667855e85b7b6f8f4c9ddc119c4939960352b
garrethil <garrethildebrandt@gmail.com>
2024-07-24 22:27:23 -0400

    update scope of the class video-title

--- client/src/pages/Vault.jsx
+++ client/src/pages/Vault.jsx
@@ -54,14 +54,14 @@ export default function Vault() {
         className="mx-5 mb-4 border-4 border-gray-300 rounded md:w-1/2"
       />
       <h2 className="text-xl font-bold my-4 p-2">Past Renderings</h2>
-      <div className="flex flex-col gap-4 w-full vault-item pb-4">
+      <div className="flex flex-col gap-4 w-full vault-item pb-4 video-title">
         {data.map((video) => (
           <div
             key={video.videoId}
             onClick={() => handleCardClick(video)}
             className="w-full p-2 cursor-pointer flex items-center justify-between lg:justify-around"
           >
-            <h3 className="text-sm video-title">{video.title}</h3>
+            <h3 className="text-sm">{video.title}</h3>
             <h4 className="text-sm hidden md:block">{video.date}</h4>
           </div>
         ))}
